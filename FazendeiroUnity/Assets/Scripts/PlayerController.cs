46848bde0d3fdbd930b248df2ad9570ebfc379b0
NoxKa <kaue.vinicius.anjos@gmail.com>
2026-03-10 18:32:36 -0300

    Versao 0.1

--- FazendeiroUnity/Assets/Scripts/PlayerController.cs
+++ FazendeiroUnity/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20f;
     public float xRange = 15f;
+    private float horizontalInput;
     public GameObject projectilePrefab;
 
     // Start is called before the first frame update
@@ -17,10 +19,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        // movimenta o player para esquerda e direita a partir da entrada do usuário
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        // movimenta o player para esquerda e direita a partir da entrada do usuï¿½rio
         transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
-        // mantém o player dentro dos limites do jogo (eixo x)
+        // mantï¿½m o player dentro dos limites do jogo (eixo x)
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.y);
@@ -29,11 +31,15 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.y);
         }
-        // dispara comida ao pressionar barra de espaço
+        // dispara comida ao pressionar barra de espaï¿½o
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
         
+    }
+    public void MoveEvent(InputAction.CallbackContext context)
+    {
+        horizontalInput = context.ReadValue<Vector2>().x;
     }
 }
