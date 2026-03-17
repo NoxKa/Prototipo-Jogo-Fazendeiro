d48196ae616b5928c01666591b5d4b8aa9669f70
NoxKa <20241ctb0180015@estudantes.ifpr.edu.br>
2026-03-17 18:25:58 -0300

    Pause

--- FazendeiroUnity/Assets/Scripts/PlayerController.cs
+++ FazendeiroUnity/Assets/Scripts/PlayerController.cs
@@ -8,17 +8,42 @@ public class PlayerController : MonoBehaviour
     public float speed = 20f;
     public float xRange = 15f;
     private float horizontalInput;
+    private InputAction pausePlayer;
+    private InputAction pauseUI;
+    public InputActionAsset InputActions;
+    private InputAction moveAction;
+    private InputAction fireAction;
+    private InputAction ghostAction;
     public GameObject projectilePrefab;
+    public GameObject pauseDisplay;
 
     // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
-        
+        pausePlayer = InputSystem.actions.FindAction("Player/Pause");
+        pauseUI = InputSystem.actions.FindAction("UI/Pause");
+        moveAction = InputSystem.actions.FindAction("Move");
+        fireAction = InputSystem.actions.FindAction("Jump");
+        ghostAction = InputSystem.actions.FindAction("Ghost");
     }
+
+
+    void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+
+    void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = moveAction.ReadValue<Vector2>().x;
         //float horizontalInput = Input.GetAxis("Horizontal");
         // movimenta o player para esquerda e direita a partir da entrada do usu�rio
         transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
@@ -31,15 +56,44 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.y);
         }
-        // dispara comida ao pressionar barra de espa�o
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (fireAction.WasPressedThisFrame())
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
-        
+        if (ghostAction.WasPressedThisFrame())
+        {
+            print("Ghost");
+        }
+        // dispara comida ao pressionar barra de espa�o
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }*/
+        DisplayPause();
     }
-    public void MoveEvent(InputAction.CallbackContext context)
+    public void DisplayPause()
+    {
+        if(pausePlayer.WasPressedThisFrame())
+        {
+            pauseDisplay.SetActive(true);
+            InputActions.FindActionMap("Player").Disable();
+            InputActions.FindActionMap("UI").Enable();
+        }else if (pauseUI.WasPressedThisFrame())
+        {
+            pauseDisplay.SetActive(false);
+            InputActions.FindActionMap("UI").Disable();
+            InputActions.FindActionMap("Player").Enable();   
+        }
+    }
+    /*public void MoveEvent(InputAction.CallbackContext context)
     {
         horizontalInput = context.ReadValue<Vector2>().x;
     }
+    public void FireAction(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }*/
 }
