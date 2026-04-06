a451ec157cdd81885403dbb84b3fd730dcfb15ac
NoxKa <kaue.vinicius.anjos@gmail.com>
2026-04-06 20:13:57 -0300

    Stopped

--- FazendeiroUnity/Assets/Scripts/Jogo/SpawnManager.cs
+++ FazendeiroUnity/Assets/Scripts/Jogo/SpawnManager.cs
@@ -15,9 +15,10 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnAnimal", startDelay, spawnInterval);
+        //InvokeRepeating("SpawnAnimal", startDelay, spawnInterval);
         player = GameObject.Find("Player");
         playerScript = player.GetComponent<PlayerController>();
+        StartCoroutine(SpawnAnimal());
     }
 
     // Update is called once per frame
@@ -32,14 +33,35 @@ public class SpawnManager : MonoBehaviour
         }
     }
 
-    void SpawnAnimal()
+    private IEnumerator SpawnAnimal()
     {
         // escolhe um animal aleatoriamente
         // animalPrefabs.Length retorna o tamanho do vetor
-        int animalIndex = Random.Range(0, animalPrefabs.Length);
         // escolhe um posição x aleatoriamente
-        Vector3 randomPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
-        Instantiate(animalPrefabs[animalIndex], randomPosition,
-            animalPrefabs[animalIndex].transform.rotation);
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+            if (!playerScript.isPaused)
+            {
+                int animalIndex = Random.Range(0, animalPrefabs.Length);
+                Vector3 randomPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+                Instantiate(animalPrefabs[animalIndex], randomPosition,
+                animalPrefabs[animalIndex].transform.rotation);
+            }
+        }
     }
+    /*private IEnumerator GerarEntulhos()
+    {
+        float genY;
+        int entulhosIndex;
+        float genTime;
+        while (true)
+        {
+            genTime = Random.Range(1, 8);
+            yield return new WaitForSeconds(genTime);
+            entulhosIndex = Random.Range(0, entulhosPrefabs.Length);
+            genY = Random.Range(-genYrange, genYrange);
+            Instantiate(entulhosPrefabs[entulhosIndex], new Vector2(genX, genY), entulhosPrefabs[entulhosIndex].transform.rotation);
+        }
+    }*/
 }
